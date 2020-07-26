89f1d7fcf76ff4f0c1797f7d2a96ca40ba44c424
Ashwin Shah <on.mony@gmail.com>
2020-07-26 18:05:01 +0530

    adding the monster code to repo

---
+++ build/precache-manifest.b7d49dd0fa8a6cb50c2ac2d7d5c1bba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ba7b3f026fd5e6bc66bbc41f5519f0",
+    "url": "/React-Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "8ddb57aa8838e549acaa",
+    "url": "/React-Monsters-Rolodex/static/css/main.786c5e94.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/React-Monsters-Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Monsters-Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ddb57aa8838e549acaa",
+    "url": "/React-Monsters-Rolodex/static/js/main.e616024f.chunk.js"
+  },
+  {
+    "revision": "026ce6368fc2823e93f3",
+    "url": "/React-Monsters-Rolodex/static/js/runtime-main.a3fae53e.js"
+  }
+]);
